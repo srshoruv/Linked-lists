c7a018ee7d761bd1fb3f35671c86fd62eb999c7f
srshoruv <srshoruv2002@gmail.com>
2024-09-06 23:00:04 +0600

    push back function

--- implementation.cpp
+++ implementation.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 class Node{
+    
+public:
     int val;
     Node* next;
-public:
+
     Node(int val) {
         this->val = val;
         next = NULL;
@@ -20,8 +22,34 @@ public:
         head = NULL;
         tail = NULL;
     }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+
+        if (head==NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+    }
+    }
 };
 
 int main(){
-    List ll();
+    List ll;
+    ll.push_front(3);
+    ll.push_back(4);
+
+    return 0;
 }
