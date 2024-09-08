61b370b730d0b641a851d934ea65dee24d10bc42
srshoruv <srshoruv2002@gmail.com>
2024-09-08 17:45:20 +0600

    doubly linked list implemented

---
+++ doublyLL.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        this->val = val;
+        next = prev = NULL;
+    }
+};
+
+class DoublyList {
+public:
+    Node* head;
+    Node* tail;
+
+    DoublyList(){
+        head = tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+        
+    }
+
+    void printList() {
+        Node* temp = head;
+
+        while (temp) {
+            cout << temp->val << " <=> ";
+            temp = temp->next;
+        } cout << "NULL \n";
+    } 
+
+};
+
+int main() {
+    DoublyList dbll;
+    dbll.push_front(4);
+    dbll.push_front(3);
+    dbll.push_front(2);
+    dbll.push_front(1);
+    dbll.printList();
+    return 0;
+}
